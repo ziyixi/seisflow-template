c87f709003d005c15decfb58ae47cfce34841053
ziyixi <ziyixi@mail.ustc.edu.cn>
2019-11-17 16:15:10 -0500

    update the git clone specfem3d_globe script

--- {{cookiecutter.project_slug}}/download_specfem.py
+++ {{cookiecutter.project_slug}}/download_specfem.py
@@ -4,7 +4,7 @@ import subprocess
 def download_specfem():
     specfem_root = "{{cookiecutter.specfem_root}}"
     specfem_url = "{{cookiecutter.specfem_url}}"
-    command = f"mkdir -p {specfem_root}"
+    command = f"git clone {specfem_url} {specfem_root}"
     subprocess.call(command, shell=True)
 
 
