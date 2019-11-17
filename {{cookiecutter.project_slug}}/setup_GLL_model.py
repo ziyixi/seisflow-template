afe1a23f5037ad1883475f6a542d92ba23334d88
ziyixi <ziyixi@mail.ustc.edu.cn>
2019-11-17 16:21:48 -0500

    fix a link related bug

--- {{cookiecutter.project_slug}}/setup_GLL_model.py
+++ {{cookiecutter.project_slug}}/setup_GLL_model.py
@@ -1,11 +1,14 @@
 import subprocess
+from os.path import join
+import os
+cwd = os.getcwd()
 
 
 def setup_GLL_model():
     if("{{cookiecutter.model_type}}" == "GLL"):
         # store the raw bin files
         gll_path = "{{cookiecutter.gll_path}}"
-        new_gll_path = "./models/raw"
+        new_gll_path = join(cwd, "models", "raw")
         command = f"cp -r {gll_path} {new_gll_path}"
         specfem_root = "{{cookiecutter.specfem_root}}"
         command = f"ln -s {new_gll_path} {specfem_root}/DATA/GLL"
